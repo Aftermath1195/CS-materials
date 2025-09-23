86485a12d4d9c91433416a8f0ebb0248b3c57215
Aftermath1195 <sourcemodeling502@gmail.com>
2025-09-23 21:27:43 +0800

    add fails

---
+++ ConsoleApp2/Work.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+if (a == 1  a == 2  a == 12) 
+           {
+    Console.WriteLine("Winter");
+}
+           else if (a == 3  a == 4  a == 5) 
+           {
+    Console.WriteLine("Spring");
+}
+           else if (a == 6  a == 7  a == 8) 
+           {
+    Console.WriteLine("Summer");
+}
+           else if (a > 12 || a < 1)
+{
+    Console.WriteLine("Ввели неправильное число");
+}
+else
+{
+    Console.WriteLine("Autumn");
+}
+
